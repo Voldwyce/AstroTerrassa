e5b8029a9025a25ceceb2da94371ba00110922f7
David <vengo53@gmail.com>
2023-11-24 16:43:08 +0100

    Initial commit

---
+++ src/main/java/com/example/astroterrassa/AstroterrassaApplication.java
@@ -0,0 +1,13 @@
+package com.example.astroterrassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroterrassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroterrassaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/astroterrassa/AstroterrassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.astroterrassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroterrassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
