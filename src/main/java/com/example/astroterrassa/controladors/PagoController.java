326fa19a4bd24cb18bbd4fe90a380da660b8de6b
Marina <81340990+Voldwyce@users.noreply.github.com>
2024-01-04 21:07:40 +0100

    Mail pago

--- src/main/java/com/example/astroterrassa/controladors/PagoController.java
+++ src/main/java/com/example/astroterrassa/controladors/PagoController.java
@@ -1,8 +1,12 @@
 package com.example.astroterrassa.controladors;
 
 import com.example.astroterrassa.model.Pago;
+import com.example.astroterrassa.model.User;
+import com.example.astroterrassa.services.EmailService;
 import com.example.astroterrassa.services.PagoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,9 @@ public class PagoController {
         this.pagoService = pagoService;
     }
 
+    @Autowired
+    private EmailService emailService;
+
     @GetMapping("/pago")
     public String getPagoForm(Model model) {
         model.addAttribute("pago", new Pago());
@@ -30,7 +37,19 @@ public class PagoController {
         } else if (pago.getProducto().equals("Normal")) {
             pago.setPrecio(40);
         }
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        User currentUser = UsuariServices.getUserByUsername(username);
+
         pagoService.savePago(pago, userId);
+        Pago savedPago = pagoService.savePago(pago, userId);
+        emailService.sendPaymentDetailsEmail(savedPago, userId);
+
         return "redirect:/index";
     }
 
--- src/main/java/com/example/astroterrassa/services/EmailService.java
+++ src/main/java/com/example/astroterrassa/services/EmailService.java
@@ -4,15 +4,21 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import com.example.astroterrassa.model.User;
+import com.example.astroterrassa.model.Pago;
+import com.example.astroterrassa.DAO.UserRepository;
 
+import java.util.Optional;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender emailSender;
+    private final UserRepository userRepository;
 
-    public EmailService(JavaMailSender emailSender) {
+    public EmailService(JavaMailSender emailSender , UserRepository userRepository) {
         this.emailSender = emailSender;
+        this.userRepository = userRepository;
+
     }
 
     public void sendWelcomeEmail(User user) {
@@ -22,4 +28,25 @@ public class EmailService {
         mail.setText("Hola " + user.getNombre() + ",\n\nBienvenido a AstroTerrassa. Estamos encantados de tenerte con nosotros.\n\nSaludos,\nEl equipo de AstroTerrassa");
         emailSender.send(mail);
     }
+
+    public void sendPaymentDetailsEmail(Pago pago, Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(user.getMail());
+            mail.setSubject("Detalles del Pago");
+            mail.setText ( "Detalles del Pago:\n" +
+                    "Nombre: " + user.getNombre() + "\n" +
+                    "Apellidos: " + user.getApellidos() + "\n" +
+                    "Email: " + user.getMail() + "\n" +
+                    "Cantidad: " + pago.getPrecio() + "\n" +
+                    "Número de Cuenta: " + pago.getCuenta() + "\n" +
+                    "Fecha: " + pago.getFechaPago() + "\n");
+
+            emailSender.send(mail);
+        }
+    }
+
 }
